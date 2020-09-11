13e33360fe58d9961bce737fe0fbfeff84d378ce
graziele-sg <jh0gf78sygs>
2020-09-11 13:21:58 -0300

    commit inicial

---
+++ src/main/java/br/com/teste/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,17 @@
+package br.com.teste.springbootadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
